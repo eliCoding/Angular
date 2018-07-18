296b4b1d23995cfc1f845a5aa380fba518c6d263
ElmiraAman <36966108+ElmiraAman@users.noreply.github.com>
2018-07-17 21:52:34 -0700

    Course component created

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // step 2 : import the component
+    CoursesComponent
   ],
   imports: [
     BrowserModule
---
+++ hello-world/src/app/courses.component.ts
@@ -0,0 +1,20 @@
+
+
+
+import {Component} from '@angular/core'
+
+
+//first step
+@Component({
+      selector:'courses',
+      template: '<h2>Courses</h2>'
+
+
+})
+
+
+
+export class CoursesComponent {
+
+
+}
