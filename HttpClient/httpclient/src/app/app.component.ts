2f5ac75db1c3e644e1c8d973ff0f5a18bc1f8d11
Elmira <e.amanollahi@gmail.com>
2018-07-26 11:10:54 -0700

    Get Json file

---
+++ HttpClient/httpclient/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent{
+  title = 'app';
+  url = 'http://angularorange.io/json/httpclientdata.json';
+  result: any;
+
+// inject httpClient object into the appComponent
+constructor(private http: HttpClient) {}
+
+ngOnInit(): void {
+  //json data will be converted to an object and we can see it on the console
+this.http.get(this.url).subscribe(data => console.log(data));
+
+}
+}
