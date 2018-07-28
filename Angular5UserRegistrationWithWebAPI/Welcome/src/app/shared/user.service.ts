b523304f1115dbfc890eade04dd70e3b694ce985
Elmira <e.amanollahi@gmail.com>
2018-07-27 18:10:10 -0700

    // add the body part which is that is from the Form for each properties

--- Angular5UserRegistrationWithWebAPI/Welcome/src/app/shared/user.service.ts
+++ Angular5UserRegistrationWithWebAPI/Welcome/src/app/shared/user.service.ts
@@ -4,6 +4,7 @@ import {Response} from "@angular/http";
 import 'rxjs/add/operator/map';
 import { User } from './user.model';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,10 +14,26 @@ export class UserService {
     //Create the base url from webapi
   readonly rootUrl = "http://localhost:55400/"
 
+
+   //inject the HttpClient into the copnstructor
   constructor(private http:HttpClient) { }
 
+  // add the body part which is that is from the Form for each properties
 registerUser (user:User) {
-  
+    //body of the request
+
+    const body: User = {
+       UserName: user.UserName,
+       Password:user.Password,
+       Email : user.Email,
+       FirstName: user.FirstName,
+       LastName: user.LastName
+
+
+    }
+
+   
+         
 }
 
 }
