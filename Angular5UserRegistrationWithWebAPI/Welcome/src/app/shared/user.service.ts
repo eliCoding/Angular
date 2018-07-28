b426058002b9bae206186bfb5d68cb4a63d164e0
Elmira <e.amanollahi@gmail.com>
2018-07-27 18:05:45 -0700

    Create the base url from the WebApi URL

--- Angular5UserRegistrationWithWebAPI/Welcome/src/app/shared/user.service.ts
+++ Angular5UserRegistrationWithWebAPI/Welcome/src/app/shared/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Response} from "@angular/http";
+import 'rxjs/add/operator/map';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+
+    //Create the base url from webapi
+  readonly rootUrl = "http://localhost:55400/"
+
+  constructor(private http:HttpClient) { }
+
+registerUser (user:User) {
+  
+}
+
 }
