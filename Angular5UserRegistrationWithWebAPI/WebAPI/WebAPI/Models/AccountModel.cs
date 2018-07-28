5b461e8ed7127f4e984993aca1457304c44cdadf
Elmira <e.amanollahi@gmail.com>
2018-07-27 17:36:19 -0700

    Accountmodel class properties

--- Angular5UserRegistrationWithWebAPI/WebAPI/WebAPI/Models/AccountModel.cs
+++ Angular5UserRegistrationWithWebAPI/WebAPI/WebAPI/Models/AccountModel.cs
@@ -7,5 +7,10 @@ namespace WebAPI.Models
 {
     public class AccountModel
     {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
