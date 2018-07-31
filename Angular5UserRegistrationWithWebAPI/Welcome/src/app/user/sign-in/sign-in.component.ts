dff957d66f3c48330d83a58021d3ef14f8f20b9e
Elmira <e.amanollahi@gmail.com>
2018-07-31 12:47:17 -0700

    ngSubmit added to sign-in.component.html

--- Angular5UserRegistrationWithWebAPI/Welcome/src/app/user/sign-in/sign-in.component.ts
+++ Angular5UserRegistrationWithWebAPI/Welcome/src/app/user/sign-in/sign-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../shared/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+    isLoginError : boolean = false;
+  constructor(private userService : UserService, private router : Router) { }
 
   ngOnInit() {
+  }
+
+
+  OnSubmit(userName,password) {
+          //if the userAuthentication will be successfull for the given user name and password it will return Json object with acces_token | token_type | expires_in
+          this.userService.userAuthentication(userName,password)
+          .subscribe ((data : any) => {
+           // store the access token in the client web browser 
+            localStorage.setItem('userToken',data.acces_token);
+            // navigate the user to the new component if the log in is successfull 
+            this.router.navigate(['/home'])
+          },
+        (err : HttpErrorResponse) => {
+              this.isLoginError = true;
+
+        } );
+        
+       
+          
+         
   }
 
 }
