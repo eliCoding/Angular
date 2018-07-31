f845ba299ce061b5da516afe2e0123ea7da51789
Elmira <e.amanollahi@gmail.com>
2018-07-30 17:07:12 -0700

    User Authentication

--- Angular5UserRegistrationWithWebAPI/WebAPI/WebAPI/Startup.cs
+++ Angular5UserRegistrationWithWebAPI/WebAPI/WebAPI/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly: OwinStartup(typeof(WebAPI.Startup))]
@@ -11,7 +13,18 @@ namespace WebAPI
     {
         public void Configuration(IAppBuilder app)
         {
-           
+            app.UseCors(CorsOptions.AllowAll);
+            OAuthAuthorizationServerOptions option = new OAuthAuthorizationServerOptions
+            {
+                TokenEndpointPath = new PathString("/token"),
+                Provider = new ApplicationOAuthProvider(),
+                AccessTokenExpireTimeSpan = TimeSpan.FromMinutes(60),
+                AllowInsecureHttp = true
+            };
+            app.UseOAuthAuthorizationServer(option);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+
         }
     }
 }
