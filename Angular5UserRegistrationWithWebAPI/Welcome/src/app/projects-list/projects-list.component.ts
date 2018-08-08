da4f8db984b5664331f2ea137dff48108c950603
Elmira <e.amanollahi@gmail.com>
2018-08-08 14:30:38 -0700

    Show All Images

--- Angular5UserRegistrationWithWebAPI/Welcome/src/app/projects-list/projects-list.component.ts
+++ Angular5UserRegistrationWithWebAPI/Welcome/src/app/projects-list/projects-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../shared/user.service';
 import { ImageService } from '../shared/image.service';
@@ -10,36 +10,36 @@ import { ImageService } from '../shared/image.service';
   styleUrls: ['./projects-list.component.css']
 })
 export class ProjectsListComponent implements OnInit {
-   userClaims: any;
-   visibleImages:any[] = [];
-  constructor(private router: Router, private userService: UserService , private imageService:ImageService) {
-         this.visibleImages = this.imageService.getImages();
-   }
+  userClaims: any;
+  visibleImages: any[] = [];
+  constructor(private router: Router, private userService: UserService, private imageService: ImageService) {
+    this.visibleImages = this.imageService.getImages();
+  }
 
-  ngOnInit() {
+      
   
+  ngOnInit() {
+
     this.userService.getUserClaims().subscribe((data: any) => {
-    
+
       this.userClaims = data;
-    
+
     });
 
- // Operation based on the role
- if(this.userService.roleMatch(['Admin']))
- {
-         //ToDo: 
-      
- }
+    // Operation based on the role
+    if (this.userService.roleMatch(['Admin'])) {
+      //ToDo: 
 
- if(this.userService.roleMatch(['Admin']))
- {
-         //ToDo: 
-      
- }
+    }
+
+    if (this.userService.roleMatch(['Developer'])) {
+      //ToDo: 
+
+    }
 
   }
 
-   
+
 
 
   Logout() {
