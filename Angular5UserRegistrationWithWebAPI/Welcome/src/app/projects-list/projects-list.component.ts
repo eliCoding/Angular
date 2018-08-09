f883759ab8054c8fa891c22c6671d4ee6b3440b4
Elmira <e.amanollahi@gmail.com>
2018-08-09 10:41:27 -0700

    UI component for project list created

--- Angular5UserRegistrationWithWebAPI/Welcome/src/app/projects-list/projects-list.component.ts
+++ Angular5UserRegistrationWithWebAPI/Welcome/src/app/projects-list/projects-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserService } from '../shared/user.service';
 import { ImageService } from '../shared/image.service';
@@ -9,15 +9,19 @@ import { ImageService } from '../shared/image.service';
   templateUrl: './projects-list.component.html',
   styleUrls: ['./projects-list.component.css']
 })
-export class ProjectsListComponent implements OnInit {
+export class ProjectsListComponent implements OnInit, OnChanges {
   userClaims: any;
+
+  @Input() filterBy?: string = 'all';
   visibleImages: any[] = [];
   constructor(private router: Router, private userService: UserService, private imageService: ImageService) {
     this.visibleImages = this.imageService.getImages();
   }
 
-      
-  
+  ngOnChanges() {
+    this.visibleImages = this.imageService.getImages();
+  }
+
   ngOnInit() {
 
     this.userService.getUserClaims().subscribe((data: any) => {
@@ -25,6 +29,8 @@ export class ProjectsListComponent implements OnInit {
       this.userClaims = data;
 
     });
+
+
 
     // Operation based on the role
     if (this.userService.roleMatch(['Admin'])) {
--- Angular5UserRegistrationWithWebAPI/Welcome/src/app/shared/image.service.ts
+++ Angular5UserRegistrationWithWebAPI/Welcome/src/app/shared/image.service.ts
@@ -23,19 +23,19 @@ export class ImageService {
 
 // TOdo : for now static image has to retrieve from Databae
 const IMAGES = [
-    { "id": 1, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 2, "category": "Sold", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 3, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 4, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 5, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 6, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 7, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 8, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 9, "category": "Sold", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 10, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 11, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 12, "category": "Sold", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 13, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 14, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" },
-    { "id": 15, "category": "Available", "caption": "xyz", "url": "/assets/image/projectPic.jpg" }
+    { "id": 1, "category": "Available", "caption": "xyz", "url": "/assets/image/p1.jpg" },
+    { "id": 2, "category": "Sold", "caption": "xyz", "url": "/assets/image/p2.jpg" },
+    { "id": 3, "category": "Available", "caption": "xyz", "url": "/assets/image/p3.jpg" },
+    { "id": 4, "category": "Available", "caption": "xyz", "url": "/assets/image/p4.jpg" },
+    { "id": 5, "category": "Available", "caption": "xyz", "url": "/assets/image/p5.jpg" },
+    { "id": 6, "category": "Available", "caption": "xyz", "url": "/assets/image/p6.jpg" },
+    { "id": 7, "category": "Available", "caption": "xyz", "url": "/assets/image/p1.jpg" },
+    { "id": 8, "category": "Available", "caption": "xyz", "url": "/assets/image/p2.jpg" },
+    { "id": 9, "category": "Sold", "caption": "xyz", "url": "/assets/image/p3.jpg" },
+    { "id": 10, "category": "Available", "caption": "xyz", "url": "/assets/image/p4.jpg" },
+    { "id": 11, "category": "Available", "caption": "xyz", "url": "/assets/image/p5.jpg" },
+    { "id": 12, "category": "Sold", "caption": "xyz", "url": "/assets/image/p6.jpg" },
+    { "id": 13, "category": "Available", "caption": "xyz", "url": "/assets/image/p2.jpg" },
+    { "id": 14, "category": "Available", "caption": "xyz", "url": "/assets/image/p3.jpg" },
+    { "id": 15, "category": "Available", "caption": "xyz", "url": "/assets/image/p1.jpg" }
 ]
