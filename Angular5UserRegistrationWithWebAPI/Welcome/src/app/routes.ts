c89ef2e9e8e91536bc67c58ecc46ee71a2a38c0d
Elmira <e.amanollahi@gmail.com>
2018-08-15 12:56:23 -0700

    Admin panel

--- Angular5UserRegistrationWithWebAPI/Welcome/src/app/routes.ts
+++ Angular5UserRegistrationWithWebAPI/Welcome/src/app/routes.ts
@@ -13,19 +13,19 @@ import { FloorPlanComponent } from './floor-plan/floor-plan.component';
 export const appRoutes: Routes = [
     { path: 'projectslist', component: ProjectsListComponent, canActivate:[AuthGuard] },
     { path: 'forbidden', component: ForbiddenComponent, canActivate:[AuthGuard] },
-    { path: 'userslist', component: UserComponent, canActivate:[AuthGuard] },
+    { path: 'userslist', component: AdminPanelComponent, canActivate:[AuthGuard] },
     { path: 'adminPanel', component: AdminPanelComponent, canActivate:[AuthGuard] ,data : {roles:['Admin']}},
-    // {
-    //     path: 'signup', component: UserComponent,
-    //     children: [{ path: '', component: SignUpComponent }]
-    // },
+    {
+        path: 'signup', component: UserComponent,
+        children: [{ path: '', component: SignUpComponent }]
+    },
 
-    // {
-    //     path: 'login', component: UserComponent,
-    //     children: [{ path: '', component: SignInComponent }]
-    // },
-    { path: 'signup', component: SignUpComponent, canActivate:[AuthGuard] },
-    { path: 'login', component: SignInComponent, canActivate:[AuthGuard] },
+    {
+        path: 'login', component: UserComponent,
+        children: [{ path: '', component: SignInComponent }]
+    },
+    // { path: 'signup', component: SignUpComponent, canActivate:[AuthGuard] },
+    // { path: 'login', component: SignInComponent, canActivate:[AuthGuard] },
 
    {   path : '', redirectTo:'login', pathMatch: 'full'},
    { path: 'image/:id', component: ProjectsImagesComponent, canActivate:[AuthGuard] },
